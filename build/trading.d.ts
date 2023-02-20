26daccae04f528d9ed32e2ddc84e691707e0a1b5
dannpl <dancardosobr@gmail.com>
2023-02-20 19:56:19 +0000

    feat: update scripts

--- build/trading.d.ts
+++
@@ -1,3 +0,0 @@
-export default class Trading {
-}
-//# sourceMappingURL=trading.d.ts.map
--- build/trading.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Trading {
-}
-exports.default = Trading;
-//# sourceMappingURL=trading.js.map
